dfd3b826938b491f89a4415f7559a5bdfc88632a
ZaidAR49 <148114243+ZaidAR49@users.noreply.github.com>
2026-04-08 17:38:15 +0300

    Loader implemented

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,13 +1,14 @@
 import ContactInfo from "@/components/contact-info";
 import { Suspense } from "react";
 import { ContactForm } from "@/components/contact-form";
+import { Loading } from "@/components/loading";
 export default function Footer() {
 
 
     return (
         <footer className="bg-[#0b1121] py-16 px-6 lg:px-24 border-t border-slate-800">
             <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-24">
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<Loading />}>
                     <ContactInfo />
                     <ContactForm />
                 </Suspense>
---
+++ src/components/loading.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Lottie from "lottie-react";
+import loading from "../assets/lottie-loading.json";
+export const Loading = () => {
+    return (
+        <div className="flex justify-center items-center h-screen ">
+            <Lottie animationData={loading}
+                loop={true}
+                style={{ width: 200, height: 200 }}
+            />
+        </div>
+    );
+};
